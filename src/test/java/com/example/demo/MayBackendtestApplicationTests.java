ebfb0f9071dc18e16c8230fc62901c55ebbb817d
PAKAMAS DETSUWAN <6310210267@psu.ac.th>
2023-08-20 20:20:42 +0700

    first commit

---
+++ src/test/java/com/example/demo/MayBackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayBackendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
